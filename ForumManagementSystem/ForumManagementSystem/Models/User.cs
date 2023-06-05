01841d2b01271203bec029e367f9b44252ac767f
GIT - FirstCommit <kristian.vaklev@yahoo.com>
2023-06-05 15:03:53 +0300

    UserClassCreated

--- ForumManagementSystem/ForumManagementSystem/Models/User.cs
+++ ForumManagementSystem/ForumManagementSystem/Models/User.cs
@@ -2,5 +2,13 @@
 {
     public class User
     {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public int Password { get; set; }
+
+
     }
 }
