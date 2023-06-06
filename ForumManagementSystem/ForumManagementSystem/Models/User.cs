2d611970e5ca1c336c5f9b87d6ae4a3b46f92c27
GIT - FirstCommit <kristian.vaklev@yahoo.com>
2023-06-06 18:49:00 +0300

    UserClassAddedBack

---
+++ ForumManagementSystem/ForumManagementSystem/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumManagementSystem.Models
+{
+    public class User
+    {
+        public User()
+        {
+            IsBlocked = false;
+        }
+        public int Id { get; set; }
+
+        [MinLength(2, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        [MaxLength(20, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        public string FirstName { get; set; }
+
+        [MinLength(2, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        [MaxLength(20, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        public string LastName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Please provide a valid email.")]
+        public string Email { get; set; }
+
+        [MinLength(5, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        [MaxLength(30, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Role Role { get; set; }
+
+        public bool IsBlocked { get; set; }
+
+
+        //public Post post { get; set; }
+
+
+    }
+}
