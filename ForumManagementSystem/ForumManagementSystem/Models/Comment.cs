282178ae09c959d5d7f01f1c7b0e1bb46106abdd
GIT - FirstCommit <kristian.vaklev@yahoo.com>
2023-06-07 12:57:16 +0300

    AdditionalFunctionalityPostClass

---
+++ ForumManagementSystem/ForumManagementSystem/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumManagementSystem.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string User { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
