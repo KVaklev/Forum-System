95551af288a9b8d30883c22e4cd568bef1469b92
Evelina Hristova <evelina.hristova.a47@learn.telerikacademy.com>
2023-06-13 17:00:19 +0300

    Added LIke to comment.

---
+++ ForumManagementSystem/Forum.Services/Contracts/ILikeCommentService.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Models;
+using ForumManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.Contracts
+{
+    public interface ILikeCommentService
+    {
+        LikeComment Create(Comment comment, User user);
+
+        LikeComment Update(Comment comment, User user);
+
+        LikeComment Delete(Comment comment, User user);
+
+        LikeComment Get(Comment comment, User user);
+    }
+}
--- ForumManagementSystem/ForumManagementSystem/Controllers/UsersApiController.cs
+++ ForumManagementSystem/ForumManagementSystem/Controllers/UsersApiController.cs
@@ -41,7 +41,7 @@ namespace ForumManagementSystem.Controllers
             {
                 User user = this.userService.GetById(id);
 
-                GetUserDto userDto = UserMapper.MapUserToDtoGet(user);
+                GetUserDto userDto = mapper.Map<GetUserDto>(user);
 
                 return this.StatusCode(StatusCodes.Status200OK, userDto);
             }
