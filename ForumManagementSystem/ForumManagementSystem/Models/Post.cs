1ed112ad97e04c8db72afe8a93fdf8268814af74
GIT - FirstCommit <kristian.vaklev@yahoo.com>
2023-06-16 10:59:20 +0300

    Readd missing files

---
+++ ForumManagementSystem/ForumManagementSystem/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿namespace ForumManagementSystem.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string User { get; set; }
+
+        public string Title { get; set; }
+
+        public string Category { get; set; } // Tag
+
+        public string Content { get; set; }
+
+        public List<Comment> Comments { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+
+        //public User users { get; set; }
+
+    }
+}
---
+++ ForumManagementSystem/ForumManagementSystem/Models/PostQueryParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace ForumManagementSystem.Models
+{
+    public class PostQueryParameters
+    {
+        public string User { get; set; }
+
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime? FromDateTime { get; set; }
+
+        public DateTime? ToDateTime { get; set; }
+
+        public string SortBy { get; set; }
+
+        public string SortOrder { get; set; }
+    }
+}
