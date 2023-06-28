7a56dbbcdc0a38d48f9913b78fc839dfc4fa98fc
Evelina Hristova <evelina.hristova.a47@learn.telerikacademy.com>
2023-06-28 20:24:58 +0300

    Added HomeView

--- ForumManagementSystem/ForumManagementSystem/Controllers/MVC/HomeController.cs
+++ ForumManagementSystem/ForumManagementSystem/Controllers/MVC/HomeController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using ForumManagementSystem.Models;
+using ForumManagementSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Helpers;
 
 namespace ForumManagementSystem.Controllers.MVC
 {
     public class HomeController : Controller
     {
+        private readonly ICategoryService categoryService;
+        private readonly IAuthManager authManager;
+
+        public HomeController(ICategoryService categoryService, IAuthManager authManager)
+        {
+            this.categoryService = categoryService;
+            this.authManager = authManager;
+        }
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            List<Category> result = categoryService.GetAll();
+            return View(result);
         }
         public IActionResult About()
         {
